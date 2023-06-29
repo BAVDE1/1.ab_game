fa8d761516784467be5121d39714b2a164bd9a3d
BAVDE1 <bavde4@gmail.com>
2023-06-29 15:49:44 +1000

    added gravity

--- puzzle_turtle.py
+++ puzzle_turtle.py
@@ -1,44 +1,65 @@
 import turtle
 
-##################
-#----Defaults----#
-##################
-
+###################
+# ----Defaults----#
+###################
 width = 30
 height = 40
 
 ground_char = "#"
+ground_block = turtle.Turtle()
+ground_block.shape("square")
+ground_block.penup()
+ground_block.speed(0)
+ground_block.color("white")
+ground_block.setposition(500, 500)
+
 player_char = "@"
-player = None
+player = turtle.Turtle()
+player.shape("circle")
+player.penup()
+player.speed(1)
+player.color("yellow")
 
 lines = []
-all_blocks = []
+all_block_pos = []  # [0]=xcor, [1]=ycor
+"""[0]=xcor, [1]=ycor"""
 drawing = False
 
 wind = turtle.Screen()
 wind.bgcolor("black")
-wind.setup(width=width * 20, height=height * 20)
+wind.setup(width=(width + 2) * 20, height=(height + 2) * 20)
 wind.title("idk")
+wind.delay(0)
 
 
-def draw_ground_cube(pos_w, pos_h):
-    obj = turtle.Turtle()
-    obj.shape("square")
-    obj.penup()
-    obj.speed(0)
-    obj.setposition(pos_w, pos_h)
-    obj.color("white")
-    all_blocks.append(obj)
+####################
+# ----Rendering----#
+####################
+def read_level():
+    with open("levels/level_1.txt") as file:
+        global lines
+        global drawing
+        lines = [line.rstrip() for line in file]
+
+        # draw level
+        drawing = True
+        wind.delay(0)
+        draw_level()
+        wind.delay(1)
+        drawing = False
+
+        # init check for ground
+        check_for_ground()
+
+
+def draw_ground_cube(pos_x, pos_y):
+    ground_block.clone().setposition(pos_x, pos_y)
+    all_block_pos.append([pos_x, pos_y])
 
 
 def draw_player(pos_w, pos_h):
-    global player
-    player = turtle.Turtle()
-    player.shape("circle")
-    player.penup()
-    player.speed(0)
     player.setposition(pos_w, pos_h)
-    player.color("yellow")
 
 
 def draw_level():
@@ -56,13 +77,30 @@ def draw_level():
                     draw_player((width / 2 * -20) + (20 * char_num), (height / 2 * 20) - (20 * line_num))
 
 
-#https://python-forum.io/thread-30979.html
+##############################
+# ----Collision & Gravity----#
+##############################
+# https://python-forum.io/thread-30979.html
 # Collision
 def check_for_ground():
-    return None
+    needs_to_fall = True
+    for block_pos in all_block_pos:
+        if block_pos[0] == player.xcor():
+            if block_pos[1] + 20 == player.ycor():
+                needs_to_fall = False
+    if needs_to_fall:
+        fall()
 
 
-# Controls
+def fall():
+    player.sety(player.ycor() - 20)
+    check_for_ground()
+    #ground_block.clone().setposition(player.xcor(), player.ycor() - 20)
+
+
+###################
+# ----Controls----#
+###################
 def left():
     print("left")
 
@@ -80,23 +118,9 @@ wind.onkeypress(left, 'Left')
 wind.onkeypress(right, 'Right')
 wind.onkeypress(space, "space")
 
-
-def read_level():
-    with open("levels/level_dat.txt") as file:
-        global lines
-        global drawing
-        lines = [line.rstrip() for line in file]
-
-        # draw level
-        drawing = True
-        draw_level()
-        drawing = False
-
-        # init check for ground
-        check_for_ground()
-
-
-# Init
+###############
+# ----Init----#
+###############
 if __name__ == '__main__':
     print("Initialising")
     read_level()
