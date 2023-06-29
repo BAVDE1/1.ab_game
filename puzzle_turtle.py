5a69638dd26a0bb7fa0c2cbbaa8e54f053c8db46
BAVDE1 <bavde4@gmail.com>
2023-06-29 16:17:31 +1000

    added wall collision

--- puzzle_turtle.py
+++ puzzle_turtle.py
@@ -25,9 +25,9 @@ player_moving = False
 
 lines = []
 all_block_pos = []  # [0]=xcor, [1]=ycor
-"""[0]=xcor, [1]=ycor"""
 drawing = False
 
+# Screen setup
 wind = turtle.Screen()
 wind.bgcolor("black")
 wind.setup(width=(width + 2) * 20, height=(height + 2) * 20)
@@ -50,9 +50,6 @@ def read_level():
         draw_level()
         wind.delay(1)
         drawing = False
-
-        # init check for ground
-        check_for_ground()
 
 
 def draw_ground_cube(pos_x, pos_y):
@@ -100,12 +97,28 @@ def fall():
     player_falling = False
 
 
+def check_for_wall(is_going_right):
+    can_move = True
+    for block_pos in all_block_pos:
+        if block_pos[1] == player.ycor():
+            if is_going_right and block_pos[0] - 20 == player.xcor():
+                can_move = False
+            elif not is_going_right and block_pos[0] + 20 == player.xcor():
+                can_move = False
+    return can_move
+
+
 ###################
 # ----Controls----#
 ###################
+left_keys = ["Left", "a"]
+right_keys = ["Right", "d"]
+interact_key = ["space"]
+
+
 def left():
     global player_moving
-    if not player_falling and not player_moving:
+    if not player_falling and not player_moving and check_for_wall(False):
         player_moving = True
         player.setx(player.xcor() - 20)
         player_moving = False
@@ -114,7 +127,7 @@ def left():
 
 def right():
     global player_moving
-    if not player_falling and not player_moving:
+    if not player_falling and not player_moving and check_for_wall(True):
         player_moving = True
         player.setx(player.xcor() + 20)
         player_moving = False
@@ -126,15 +139,26 @@ def space():
 
 
 wind.listen()
-wind.onkeypress(left, 'Left')
-wind.onkeypress(right, 'Right')
-wind.onkeypress(space, "space")
+wind.onkeypress(left, left_keys[0])
+wind.onkeypress(left, left_keys[1])
+wind.onkeypress(right, right_keys[0])
+wind.onkeypress(right, right_keys[1])
+wind.onkeypress(right, interact_key[0])
 
 ###############
 # ----Init----#
 ###############
 if __name__ == '__main__':
     print("Initialising")
+
+    # draw level
+    print("Drawing level")
     read_level()
+
+    # init check for ground
+    print("Checking for ground")
+    check_for_ground()
+
+    print("Initialisation complete")
 
 wind.mainloop()
